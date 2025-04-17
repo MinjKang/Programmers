1fa35baf12866b2c9bf1f26014fda8fddf71486d
Minjung Kang <49437468+MinjKang@users.noreply.github.com>
2025-04-17 15:44:35 +0900

    [level 1] Title: 명예의 전당 (1), Time: 4.48 ms, Memory: 9.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -0,0 +1,19 @@
+def solution(k, score):
+    answer = []
+    min_answer_list = []
+    
+    for s in score:
+        answer.append(s)
+        min_answer = min(answer)
+        
+        if len(answer)>k:
+            answer = sorted(answer)
+            answer = (answer[-k:])
+            min_answer = min(answer)
+        
+        min_answer_list.append(min_answer)
+        
+    return min_answer_list
+        
+        
+    
