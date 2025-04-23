3be23153c6f6917c1ec74c4daa53c56094ef2c77
Minjung Kang <49437468+MinjKang@users.noreply.github.com>
2025-04-23 23:34:05 +0900

    [level 1] Title: 폰켓몬, Time: 0.81 ms, Memory: 9.69 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,9 +1,21 @@
 def solution(nums):
-    answer = 0
-    answer = len(set(nums))
+    answer = len(nums)/2
+    if len(set(nums))<answer:
+        answer = len(set(nums))
+    return answer
     
-    if answer > len(nums)/2:
-        answer = len(nums)/2
+    
+    
+    
+    
+    
+    
+    
+#     answer = 0
+#     answer = len(set(nums))
+    
+#     if answer > len(nums)/2:
+#         answer = len(nums)/2
 
-    return(int(answer))
+#     return(int(answer))
     
