103be59b0609ceec3ad7d03e0bd4dfba0e68653f
Minjung Kang <49437468+MinjKang@users.noreply.github.com>
2025-05-07 12:31:56 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+def solution(array, n):
+    return (Counter(array)[n])
+#     result = 0
+    
+#     for i in range(len(array)):
+#         if array[i] == n:
+#             result +=1
+            
+    # return result
