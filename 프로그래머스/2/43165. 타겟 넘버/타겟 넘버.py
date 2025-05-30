4dfec1e19fec011b99bf63caf205caa6e0af5519
Minjung Kang <49437468+MinjKang@users.noreply.github.com>
2025-05-30 16:45:24 +0900

    [level 2] Title: 타겟 넘버, Time: 584.00 ms, Memory: 98.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(numbers, target):
+    queue = deque()
+    queue.append((0, 0))  
+
+    count = 0  
+
+    while queue:
+        current_sum, index = queue.popleft()
+
+        if index == len(numbers):
+            if current_sum == target:
+                count += 1
+        else:
+            queue.append((current_sum + numbers[index], index + 1))
+            queue.append((current_sum - numbers[index], index + 1))
+
+    return count
