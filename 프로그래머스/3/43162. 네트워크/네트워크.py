bc76fe48e15b125669bd012bdb763e27ca9d9b17
Minjung Kang <49437468+MinjKang@users.noreply.github.com>
2025-06-01 17:11:18 +0900

    [level 3] Title: 네트워크, Time: 0.50 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,18 +1,21 @@
+from collections import deque
+
 def solution(n, computers):
-    visited = [False] * n  
-    count = 0  
-
+    visited = [False] * n
+    count = 0
+    
     for i in range(n):
-        if not visited[i]:  
-            stack = [i] 
-
-            while stack:
-                now = stack.pop()
-                visited[now] = True 
-
+        if not visited[i]: 
+            queue = deque([i])
+            # print(queue)
+            visited[i] = True
+        
+            while queue:
+                now = queue.popleft()
                 for j in range(n):
-                    if computers[now][j] == 1 and not visited[j]:
-                        stack.append(j)
-
-            count += 1  
+                    if computers[now][j] == 1 and visited[j]==False:
+                        # print(i, j)
+                        queue.append(j)
+                        visited[j] = True
+            count+=1    
     return count
